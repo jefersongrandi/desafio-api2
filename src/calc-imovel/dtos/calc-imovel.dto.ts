c87d85d7767f54892d94663f520c667dece32751
Jeferson <jeferson_grandi@yahoo.com.br>
2021-06-21 23:50:59 -0300

    corrigindo validação de qtde

--- src/calc-imovel/dtos/calc-imovel.dto.ts
+++ src/calc-imovel/dtos/calc-imovel.dto.ts
@@ -3,8 +3,8 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CalculoImovelDTO {
 
-  @IsNotEmpty({message: "qtde_metros não pode ser nulo."})
-  @IsInt({message: "qtde_metros deve ser um número"})
+  @IsNotEmpty({message: "Valor para 'Quantidade de metros' não pode ser nulo."})
+  @IsInt({message: "Valor para 'Quantidade de metros' deve ser um número"})
   @ApiProperty({ minimum: 10, maximum: 10000, example: 10, description: 'Quantidade de metros quadrados' })
   qtdeMetros: number;
 
--- src/calc-imovel/pipes/calc-imovel.validation.pipe.ts
+++ src/calc-imovel/pipes/calc-imovel.validation.pipe.ts
@@ -6,8 +6,8 @@ export class CalcImovelValidationClassPipe implements PipeTransform {
 
   transform(body: any, metaData: ArgumentMetadata): number {
 
-    let qtde_metros = body.qtde_metros;
-    if (qtde_metros < 10 || qtde_metros > 10000) throw new BadRequestException(`A quantidade de metros de estar entre 10 e 10.000`);
+    let qtdeMetros = body.qtdeMetros;
+    if (qtdeMetros < 10 || qtdeMetros > 10000) throw new BadRequestException(`A quantidade de metros de estar entre 10 e 10.000`);
     return body;
 
   }
